e724a7b1782163e13d8d71abce5dc1353a97fa28
Donel21 <162301589+Donel21@users.noreply.github.com>
2024-06-09 21:56:33 +0200

    Add files via upload

---
+++ mainscreen.kt
@@ -0,0 +1,11 @@
+package com.example.imadpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mainscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mainscreen)
+    }
+}
